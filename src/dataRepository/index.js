42cc3c473360bfbd783f230721b41fb3b4e85ad0
ilyaqwerty <topmastertop97@gmail.com>
2019-06-30 02:16:53 +0300

    add cors proxying for dev and direct path for production

--- src/dataRepository/index.js
+++ src/dataRepository/index.js
@@ -1,7 +1,26 @@
 const PATH = process.env.REACT_APP_PATH;
 
+function get (url, ...params) {
+  console.log('GET', url);
+
+  const GET = {
+    method: 'GET',
+    ...params
+  };
+
+  return fetch(url, GET)
+    .then(res => {
+      console.log(res);
+      return res.json()
+    })
+    .catch(e => {
+      console.log(e);
+      throw e
+    })
+}
+
 export default class Data {
-  getJobsList() {
-    return Promise.resolve({})
+  getJobsList (...args) {
+    return get(PATH, ...args)
   }
 }
--- src/ui/containers/Root.js
+++ src/ui/containers/Root.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
+import {getJobsList} from '../../model/actions'
 import Form from './Form'
 import { Container } from '../styled'
 
 export default class Root extends Component {
+  constructor(props){
+    super(props);
+    getJobsList({})
+  }
   render () {
     return <>
       <Container>
