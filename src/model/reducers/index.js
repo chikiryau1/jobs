9f96a8a98e30d8041763035fd92f47ab701460f1
ilyaqwerty <topmastertop97@gmail.com>
2019-06-30 17:38:14 +0300

    asd

--- src/model/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import jobList from './jobList'
-import job from './job'
-import form from './form'
-
-
-export { jobList, job, form }
--- src/ui/containers/index.js
+++
@@ -1,4 +0,0 @@
-import Root from './Root'
-import Job from './Job'
-
-export { Root, Job }
